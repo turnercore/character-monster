d8eae94f3be4b5e25e637bee3241df823aab3a2a
Turner Monroe <me@turnercore.dev>
2024-01-01 18:57:39 +0000

    updated tables

--- character-monster/lib/constants.ts
+++ character-monster/lib/constants.ts
@@ -2,7 +2,7 @@
 
 const projectName = 'character-monster'
 //VERY IMPORTANT: Make sure this identifier is unique if you're using shared databases or you will overrwrite your old database
-const projectPrefix = 'cm' 
+const projectPrefix = 'cm'
 
 // See if database is shared
 const isSharedDb = process.env.SHARED_DB ? process.env.SHARED_DB : false
@@ -13,6 +13,17 @@ const hasSharedProfiles = process.env.SHARED_PROFILES
 // Tables in DB (if in a shared db then they will be prefixed automatically)
 export const PROFILES_TABLE =
   hasSharedProfiles || !isSharedDb ? 'profiles' : projectPrefix + '_profiles'
-export const API_KEY_TABLE = isSharedDb
+
+export const API_KEYS_TABLE = isSharedDb
   ? projectPrefix + '_api_keys'
   : 'api_keys'
+
+export const BLURBS_TABLE = isSharedDb ? projectPrefix + '_blurbs' : 'blurbs'
+
+export const CHARACTERS_TABLE = isSharedDb
+  ? projectPrefix + '_characters'
+  : 'characters'
+
+export const THIRD_PARTY_KEYS_TABLE = isSharedDb
+  ? projectPrefix + '_third_party_keys'
+  : 'third_party_keys'
