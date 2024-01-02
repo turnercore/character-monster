17e4473e014be642fd2285f9922e492d535a31b4
Turner Monroe <me@turnercore.dev>
2024-01-02 02:01:25 +0000

    Formatting of UI on Login Page

--- character-monster/app/account/login/page.tsx
+++ character-monster/app/account/login/page.tsx
@@ -2,6 +2,14 @@ import Link from 'next/link'
 import { headers, cookies } from 'next/headers'
 import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui'
 
 export default function Login({
   searchParams,
@@ -55,45 +63,54 @@ export default function Login({
   }
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <form
-        className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-        action={signIn}
-      >
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-          Sign In
-        </button>
-        <button
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-        >
-          Sign Up
-        </button>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
-    </div>
+    <Card className="max-w-[500px] mx-auto mt-10">
+      <CardHeader className="items-center">
+        <CardTitle>Login / SignUp</CardTitle>
+        <CardDescription></CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 mx-auto">
+          <form
+            className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+            action={signIn}
+          >
+            <label className="text-md" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="rounded-md px-4 py-2 bg-inherit border mb-6"
+              name="email"
+              placeholder="you@example.com"
+              required
+            />
+            <label className="text-md" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="rounded-md px-4 py-2 bg-inherit border mb-6"
+              type="password"
+              name="password"
+              placeholder="••••••••"
+              required
+            />
+            <Button type="submit" className="rounded-md px-4 py-2 mb-2">
+              Sign In
+            </Button>
+            <Button
+              formAction={signUp}
+              variant="secondary"
+              className="rounded-md px-4 py-2 mb-2"
+            >
+              Sign Up
+            </Button>
+            {searchParams?.message && (
+              <p className="mt-4 p-4 bg-foreground/10 text-center hover:">
+                {searchParams.message}
+              </p>
+            )}
+          </form>
+        </div>
+      </CardContent>
+    </Card>
   )
 }
