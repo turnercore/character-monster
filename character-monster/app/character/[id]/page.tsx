74b26516a325b9833404275167b8da6223a5402f
Turner Monroe <me@turnercore.dev>
2024-01-03 00:03:09 +0000

    added blank page so vercel can deploy

--- character-monster/app/character/[id]/page.tsx
+++ character-monster/app/character/[id]/page.tsx
@@ -1,1 +1,10 @@
 // TODO
+import { NextPage } from 'next'
+
+const CharacterPage: NextPage = () => {
+  // TODO: Add your code here
+
+  return <div>{/* TODO: Add your JSX here */}</div>
+}
+
+export default CharacterPage
